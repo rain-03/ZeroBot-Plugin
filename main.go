bd2051b9857cebff1882f6029d27a3825fe23c2e
rain-03 <149313395+rain-03@users.noreply.github.com>
2023-10-29 22:28:52 +0800

    Update main.go

--- main.go
+++ main.go
@@ -36,7 +36,7 @@ import (
 
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/sleepmanage" // 统计睡眠时间
 
-	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/atri" // ATRI词库
+	//_ "github.com/FloatTech/ZeroBot-Plugin/plugin/atri" // ATRI词库
 
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/manager" // 群管
 
